3d9af88e0d5cf54ed6968825ebdaaedbc33ea4c8
TophUwU <tophuwo01@gmail.com>
2022-08-14 15:19:17 +0200

    - ADD documentation for SALv2 annotations

--- Marble Engine/include/def.h
+++ Marble Engine/include/def.h
@@ -10,15 +10,42 @@
 
 /*
  * Shorten some very long SAL annotations, improving
- * readability.
+ * readability. Also used for combining annotations in a more
+ * readable and shorter form.
  */
 #if (defined MB_DYNAMIC_LIBRARY)
-	#define _Init_(param)      _Post_satisfies_(*param != NULL) _On_failure_(_Post_satisfies_(param != NULL ? (*param == NULL) : 1))
-	#define _Uninit_(param)    _Post_satisfies_(*param == NULL)
-	#define _Success_ok_       _Success_(return == MARBLE_EC_OK)
-	#define _Critical_         _Check_return_ _Success_ok_
-	#define _Size_(size)       _Out_writes_bytes_(size)
-	#define _Maybe_valid_      _Outptr_result_maybenull_
+	/*
+	 * Used for functions that allocate and initialize
+	 * an "object"; these functions are usually named
+	 * "*_create()".
+	 */
+	#define _Init_(param)   _Post_satisfies_(*param != NULL) _On_failure_(_Post_satisfies_(param != NULL ? (*param == NULL) : 1))
+	/*
+	 * Used for functions that destroy an "object" and
+	 * release all of its resources it occupied. These
+	 * functions act as counterparts to "*_create()"
+	 * and are appropriately named "*_destroy()".
+	 */
+	#define _Uninit_(param) _Post_satisfies_(*param == NULL)
+	/*
+	 * Denotes functions that return a value of type "marble_ecode_t"
+	 * what exact value denotes a successful completion.
+	 */
+	#define _Success_ok_    _Success_(return == MARBLE_EC_OK)
+	/*
+	 * A combination of '_Success_ok_' and an annotation
+	 * throwing a warning if the caller does not inspect
+	 * the function's return value. Used for "*_create()"
+	 * functions, for instance.
+	 */
+	#define _Critical_      _Check_return_ _Success_ok_
+	/*
+	 * Denoting that a parameter has to be of **size** bytes
+	 * at the very least.
+	 */
+	#define _Size_(size)    _Out_writes_bytes_(size)
+	/* Just shortening a long annotation. */
+	#define _Maybe_valid_   _Outptr_result_maybenull_
 #endif
 
 
