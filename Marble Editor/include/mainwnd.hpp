a51cc0c2444e2c9950fa86befea26ed26d812be2
TophUwO <tophuwo01@gmail.com>
2022-10-12 22:23:51 +0200

    ADD main app icon + license
    - Now use spaces instead of tabs for intendation

--- Marble Editor/include/mainwnd.hpp
+++ Marble Editor/include/mainwnd.hpp
@@ -4,19 +4,19 @@
 
 
 namespace mbe {
-	class mainwindow : public QMainWindow {
-		Q_OBJECT
-
-	public:
-		mainwindow(QSize const &cr_dims, QWidget *cp_parent = nullptr);
-		~mainwindow();
-
-	private:
-		void int_createmenubar();
-		void int_createwidgets();
-
-		QMenuBar *mw_menubar;
-	};
+    class mainwindow : public QMainWindow {
+        Q_OBJECT
+    
+    public:
+        mainwindow(QSize const &cr_dims, QWidget *cp_parent = nullptr);
+        ~mainwindow();
+    
+    private:
+        void int_createmenubar();
+        void int_createwidgets();
+    
+        QMenuBar *mw_menubar;
+    };
 }
 
 
--- Marble Editor/src/mainwnd.cpp
+++ Marble Editor/src/mainwnd.cpp
@@ -4,40 +4,41 @@
 
 
 namespace mbe {
-	mainwindow::mainwindow(QSize const &cr_dims, QWidget *cp_parent)
-		: QMainWindow(cp_parent)
-	{
-		/* Set main window properties. */
-		resize(cr_dims.width(), cr_dims.height());
+    mainwindow::mainwindow(QSize const &cr_dims, QWidget *cp_parent)
+        : QMainWindow(cp_parent)
+    {
+        /* Set main window properties. */
+        resize(cr_dims.width(), cr_dims.height());
+        setWindowIcon(QIcon(":/icons/ico_appmain.ico"));
+        
+        /* Create child widgets. */
+        int_createmenubar();
+        int_createwidgets();
+    }
+    
+    mainwindow::~mainwindow() {
 
-		/* Create child widgets. */
-		int_createmenubar();
-		int_createwidgets();
-	}
+    }
+    
+    void mainwindow::int_createmenubar() {
+        /* Create menubar. */
+        mw_menubar = new QMenuBar(this);
+        
+        /* Create and setup sub-menus. */
+        QMenu   *cp_mfile     = new QMenu("File", mw_menubar);
+        QAction *cp_afileexit = new QAction("Exit", cp_mfile);
+        
+        cp_mfile->addAction(cp_afileexit);
+        
+        /* Associate menubar with main-window. */
+        mw_menubar->addAction(cp_mfile->menuAction());
+        
+        setMenuBar(mw_menubar);
+    }
+    
+    void mainwindow::int_createwidgets() {
 
-	mainwindow::~mainwindow() {
-
-	}
-
-	void mainwindow::int_createmenubar() {
-		/* Create menubar. */
-		mw_menubar = new QMenuBar(this);
-
-		/* Create and setup sub-menus. */
-		QMenu   *cp_mfile     = new QMenu("File", mw_menubar);
-		QAction *cp_afileexit = new QAction("Exit", cp_mfile);
-		
-		cp_mfile->addAction(cp_afileexit);
-
-		/* Associate menubar with main-window. */
-		mw_menubar->addAction(cp_mfile->menuAction());
-
-		setMenuBar(mw_menubar);
-	}
-
-	void mainwindow::int_createwidgets() {
-	
-	}
+    }
 }
 
 
